2e03d21c2dbf5a2c645fad534b0812782c560124
vaabot <111668638+vaabot@users.noreply.github.com>
2022-08-28 17:44:34 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -8,11 +8,11 @@ let handler = async (m, { conn }) => {
     let txt = `
 Bot ini menggunakan script github
 
-https://github.com/FokusDotId/Family-MD.git
+https://github.com/vaabot cari sendiri😛
 
 pliss follow dan kasih ✨⭐⭐🌟 ya pak!
 
-My Github: https://github.com/FokusDotId
+My Github: https://github.com/vaabot
 
 `
      conn.reply(m.chat, txt, m)
